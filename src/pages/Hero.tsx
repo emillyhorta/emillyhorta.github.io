903ccafe7413efc542408101946f65447ec0583f
Emilly <emillyhortapy@gmail.com>
2025-07-10 13:21:46 -0300

    Update Hero.tsx

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -19,7 +19,7 @@ export default function Hero() {
           Emilly Horta
         </p>
         <h1 className="mb-8 text-center text-4xl font-bold text-white sm:text-5xl md:mb-12 md:text-6xl">
-          Front-end Developer & UI Designer
+          UX/UI Designer
         </h1>
 
         <div className="flex w-full flex-col gap-2.5 sm:flex-row sm:justify-center">
